eed534c3c0d4ae4b1973e2038e0f856d07c46486
Jordan <jordan@daydream-lab.com>
2019-03-18 15:20:42 +0800

    fixed bug

--- src/database/seeds/LanguagesTableSeeder.php
+++ src/database/seeds/LanguagesTableSeeder.php
@@ -30,14 +30,11 @@ class LanguagesTableSeeder extends Seeder
 
 
         Language::create([
-            'title'     => env('APP_NAME').' - 繁體中文',
-            'sitename'  => env('APP_NAME'),
-            'site_id'   => 1,
-            'code'      => 'zh-TW',
+            'title'     => '繁體中文',
+            'type'      => 'content',
+            'code'      => 'zh-tw',
             'sef'       => 'tw',
             'image'     => 'zh_tw',
-            'state'     => 1,
-            'access'    => 1,
             'created_by'=> 1
         ]);
 
