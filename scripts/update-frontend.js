9753d50d1fa4c70622731e253c96bc5a244902f7
littlegreening <littlegreening@gmail.com>
2019-04-25 11:13:31 +0800

    add node.js script for updating frontend assets
    npm run update-frontend
    configuration file: config.json

---
+++ scripts/update-frontend.js
@@ -0,0 +1,45 @@
+let abs = require("path").resolve;
+let fs = require("fs-extra");
+let alert = require("./alert");
+
+const project = abs(__dirname, "../");
+// Load configuration
+const config = fs.readJsonSync("./scripts/config.json");
+
+let updatePublicDir = callback => {
+  //Empty public directory
+  fs.emptyDirSync(abs(project, config.targetPublicDirPath));
+
+  //Copy files from source directory
+  fs.copy(
+    abs(project, config.sourceDirPath),
+    abs(project, config.targetPublicDirPath),
+    err => {
+      if (err) return alert(err);
+      callback();
+    }
+  );
+};
+
+let updateIndexFile = callback => {
+  //Empty resources admin directory
+  fs.emptyDirSync(abs(project, config.targetViewPath));
+
+  //Copy files from source directory
+  fs.copy(
+    abs(project, config.sourceViewPath),
+    abs(project, config.targetViewPath),
+    err => {
+      if (err) return alert(err);
+      callback();
+    }
+  );
+};
+
+// Run scripts
+alert("Start update frontend assets...");
+updatePublicDir(() => {
+  updateIndexFile(() => {
+    alert("Updating frontend assets is completed!");
+  });
+});
