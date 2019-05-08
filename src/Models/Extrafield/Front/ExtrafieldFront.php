2ae5147a9b95cd52aabc10c6a9e6c882a1f76b82
Jordan <jordan@daydream-lab.com>
2019-05-08 18:44:58 +0800

    update

--- src/Models/Extrafield/Front/ExtrafieldFront.php
+++ src/Models/Extrafield/Front/ExtrafieldFront.php
@@ -22,7 +22,9 @@ class ExtrafieldFront extends Extrafield
         'created_by',
         'updated_by',
         'created_at',
-        'updated_at'
+        'updated_at',
+        'locked_by',
+        'locked_at'
     ];
 
 
