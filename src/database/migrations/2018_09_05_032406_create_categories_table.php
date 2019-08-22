67882c90597c1b9902310f99ebabbfe99f7f079c
Jordan <jordan@daydream-lab.com>
2019-08-22 12:49:14 +0800

    update

--- src/database/migrations/2018_09_05_032406_create_categories_table.php
+++ src/database/migrations/2018_09_05_032406_create_categories_table.php
@@ -30,7 +30,7 @@ class CreateCategoriesTable extends Migration
             $table->unsignedInteger('hits')->nullable()->default(0);
             $table->unsignedInteger('access');
 
-            $table->string('language')->default('*');
+            $table->string('language')->nullable()->default('*');
             $table->string('template')->nullable();
             $table->string('metadesc')->nullable();
             $table->string('metakeywords')->nullable();
