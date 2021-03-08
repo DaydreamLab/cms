b8259d2170a8c430686880f4f78003e8b276a067
Jerry <Jerry@daydream-lab.com>
2021-03-08 17:58:41 +0800

    fix

--- src/Services/Extrafield/Admin/ExtrafieldAdminService.php
+++ src/Services/Extrafield/Admin/ExtrafieldAdminService.php
@@ -5,6 +5,7 @@ namespace DaydreamLab\Cms\Services\Extrafield\Admin;
 use DaydreamLab\Cms\Repositories\Extrafield\Admin\ExtrafieldAdminRepository;
 use DaydreamLab\Cms\Services\Extrafield\ExtrafieldService;
 use DaydreamLab\JJAJ\Traits\LoggedIn;
+use DaydreamLab\JJAJ\Helpers\InputHelper;
 use Illuminate\Support\Collection;
 
 class ExtrafieldAdminService extends ExtrafieldService
