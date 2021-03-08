915c32c1363ab75b0ffd589506a81c681705d81d
aass5050 <aass5050@gmail.com>
2021-03-08 16:58:15 +0800

    split category item key alias

--- src/Resources/Menus/Front/Models/MenuResource.php
+++ src/Resources/Menus/Front/Models/MenuResource.php
@@ -2,11 +2,15 @@
 
 namespace DaydreamLab\Cms\Resources\Menus\Front\Models;
 
+use DaydreamLab\Cms\Models\Category\Front\CategoryFront;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Str;
 
 class MenuResource extends JsonResource
 {
+    private $hantTitles = [];
+
     /**
      * Transform the resource into an array.
      *
@@ -15,10 +19,14 @@ class MenuResource extends JsonResource
      */
     public function toArray($request)
     {
+        $items = is_countable($this->items)
+            ? $this->formatItems($this->items)
+            : $this->items;
+
         return [
             'title'          => $this->title,
             'alias'          => $this->alias,
-            'items'          => $this->items,
+            'items'          => $items,
             'description'    => $this->description,
             'creator'        => $this->creator,
             'updater'        => $this->updater,
@@ -28,5 +36,51 @@ class MenuResource extends JsonResource
             'access_title'   => $this->access_title,
             'language_title' => $this->language_title,
         ];
+    }
+
+
+    protected function formatItems($items)
+    {
+        $result = [];
+
+        $storageKeys = [];
+
+        foreach ($items as $alias => $data) {
+            $key = $alias;
+            if ($data instanceof LengthAwarePaginator) {
+                $title  = $this->getTitle($alias, $this->language);
+                $data   = $data->toArray();
+                $values = $data['data'];
+                unset($data['data']);
+                $paginate = $data;
+
+                $result[$key] = [
+                    'title'    => $title,
+                    'items'    => $values,
+                    'paginate' => $paginate
+                ];
+            }
+        }
+
+        return $result ?: $items;
+    }
+
+    private function getTitle($alias, $lang)
+    {
+        if ($lang === 'en') {
+            return Str::ucfirst($alias);
+        }
+
+        if ($alias == 'all') {
+            return '所有友站';
+        }
+
+        if (!isset($this->hantTitles[$alias])) {
+            $this->hantTitles[$alias] = CategoryFront::where('alias', $alias)
+                                                     ->where('language', $lang)
+                                                     ->first()->title;
+        }
+        
+        return $this->hantTitles[$alias];
     }
 }
