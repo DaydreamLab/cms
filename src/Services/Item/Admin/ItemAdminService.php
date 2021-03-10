35f121c3bbf39f04bbcaa601915532abbb2a941f
aass5050 <aass5050@gmail.com>
2021-03-10 13:55:53 +0800

    feat 增加搜尋content_type預設類型

--- src/Services/Item/Admin/ItemAdminService.php
+++ src/Services/Item/Admin/ItemAdminService.php
@@ -5,13 +5,12 @@ namespace DaydreamLab\Cms\Services\Item\Admin;
 use DaydreamLab\Cms\Repositories\Item\Admin\ItemAdminRepository;
 use DaydreamLab\Cms\Services\Category\Admin\CategoryAdminService;
 use DaydreamLab\Cms\Services\Cms\CmsCronJobService;
+use DaydreamLab\Cms\Services\Item\ItemService;
 use DaydreamLab\Cms\Services\Tag\Admin\TagAdminService;
 use DaydreamLab\Cms\Traits\Service\CmsCronJob;
 use DaydreamLab\JJAJ\Helpers\Helper;
 use DaydreamLab\JJAJ\Helpers\InputHelper;
-use DaydreamLab\Cms\Services\Item\ItemService;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
 
 class ItemAdminService extends ItemService
 {
@@ -29,25 +28,25 @@ class ItemAdminService extends ItemService
 
     protected $search_keys = ['title', 'introtext', 'description', 'extrafields_search'];
 
-    public function __construct(ItemAdminRepository     $repo,
-                                TagAdminService         $tagAdminService,
-                                ItemTagMapAdminService  $itemTagMapAdminService,
-                                CategoryAdminService    $categoryAdminService,
-                                CmsCronJobService       $cmsCronJobService)
+    public function __construct(
+        ItemAdminRepository $repo,
+        TagAdminService $tagAdminService,
+        ItemTagMapAdminService $itemTagMapAdminService,
+        CategoryAdminService $categoryAdminService,
+        CmsCronJobService $cmsCronJobService)
     {
         parent::__construct($repo);
-        $this->repo                     = $repo;
-        $this->tagAdminService          = $tagAdminService;
-        $this->itemTagMapAdminService   = $itemTagMapAdminService;
-        $this->categoryAdminService     = $categoryAdminService;
-        $this->cmsCronJobService        = $cmsCronJobService;
+        $this->repo                   = $repo;
+        $this->tagAdminService        = $tagAdminService;
+        $this->itemTagMapAdminService = $itemTagMapAdminService;
+        $this->categoryAdminService   = $categoryAdminService;
+        $this->cmsCronJobService      = $cmsCronJobService;
     }
 
 
     public function add(Collection $input)
     {
-        if ((int)$input->get('featured') == 1)
-        {
+        if ((int) $input->get('featured') == 1) {
             $newest = $this->repo->findNewestFeatured();
             $input->put('featured_ordering', $newest ? $newest->featured_ordering + 1 : 1);
         }
@@ -59,17 +58,13 @@ class ItemAdminService extends ItemService
     public function modify(Collection $input, $diff = false)
     {
         $input_featured = $input->get('featured');
-        $item = $this->checkItem($input->id, $diff);
+        $item           = $this->checkItem($input->id, $diff);
         // 代表有修改到 featured 值
-        if ($item && $item->featured != $input_featured)
-        {
-            if ((int)$input_featured == 1)
-            {
+        if ($item && $item->featured != $input_featured) {
+            if ((int) $input_featured == 1) {
                 $newest = $this->repo->findNewestFeatured();
                 $input->put('featured_ordering', $newest->featured_ordering++);
-            }
-            else
-            {
+            } else {
                 $newer = $this->repo->findNewerFeatured($item);
                 $this->repo->updateOrdering($newer, '--');
                 $input->put('featured_ordering', null);
@@ -81,35 +76,21 @@ class ItemAdminService extends ItemService
 
     public function search(Collection $input)
     {
-        $extension      = !InputHelper::null($input, 'extension') ? $input->get('extension') : 'item';
-        if ($extension == 'item')
-        {
-            if (InputHelper::null($input, 'content_type'))
-            {
-                $content_type = 'article';
+        $extension = $input->get('extension') ?: 'item';
 
-            }
-            else
-            {
-                $content_type = $input->get('content_type');
-            }
-        }
-        else
-        {
+        if ($extension == 'item') {
+            $content_type = $input->get('content_type') ?: ['item', 'article', 'link', 'slideshow'];
+        } else {
             $content_type = '';
         }
 
-
-        if (!InputHelper::null($input, 'category_id'))
-        {
+        if ($input->get('category_id')) {
             $category_ids = $this->categoryAdminService->findSubTreeIds($input->category_id);
-        }
-        else
-        {
+        } else {
             $categories = $this->categoryAdminService->search(Helper::collect([
-                'extension'     => $extension,
-                'content_type'  => $content_type,
-                'paginate'  => false
+                'extension'    => $extension,
+                'content_type' => $content_type,
+                'paginate'     => false
             ]));
 
             $category_ids = $categories->map(function ($item, $key) {
@@ -117,12 +98,13 @@ class ItemAdminService extends ItemService
             });
         }
 
-        $input->put('special_queries',
-            [[
-                'type'  => 'whereIn',
-                'key'   => 'category_id',
-                'value' => $category_ids
-            ]]
+        $input->put('special_queries', [
+                [
+                    'type'  => 'whereIn',
+                    'key'   => 'category_id',
+                    'value' => $category_ids
+                ]
+            ]
         );
 
         // Item Search 沒有這兩個 column
@@ -137,13 +119,11 @@ class ItemAdminService extends ItemService
 
     public function store(Collection $input, $diff = false)
     {
-        if (InputHelper::null($input, 'hits'))
-        {
+        if (InputHelper::null($input, 'hits')) {
             $input->put('hits', 0);
         }
 
-        if ($input->state == 1 && InputHelper::null($input, 'publish_up'))
-        {
+        if ($input->state == 1 && InputHelper::null($input, 'publish_up')) {
             $input->put('publish_up', now());
             $input->publish_up = now()->toDateTimeString();
         }
@@ -151,22 +131,16 @@ class ItemAdminService extends ItemService
         $tags = $input->get('tags') ? $input->get('tags') : [];
         $input->forget('tags');
 
-        $result    =  parent::store($input, $diff);
+        $result = parent::store($input, $diff);
 
-        if (gettype($result) == 'boolean')
-        {
-            if ($result === true)
-            {
+        if (gettype($result) == 'boolean') {
+            if ($result === true) {
                 $item = $this->find($input->id);
-            }
-            else
-            {
+            } else {
                 // Something error alias重複
                 return $this->response;
             }
-        }
-        else
-        {
+        } else {
             $item = $this->find($result->id);
         }
 
@@ -180,41 +154,31 @@ class ItemAdminService extends ItemService
     }
 
 
-
     public function storeTags($tags = [], $item_id)
     {
         $tag_ids = [];
-        foreach ($tags  as $tag)
-        {
-            if (array_key_exists('id', $tag) && $tag['id'] != '')
-            {
+        foreach ($tags as $tag) {
+            if (array_key_exists('id', $tag) && $tag['id'] != '') {
                 //$tag_ids[] = $tag['id'];
-            }
-            else if(array_key_exists('title', $tag))
-            {
-                $db_tag = $this->tagAdminService->findBy('title', '=', $tag['title'])->first();
-                if (!$db_tag)
-                {
+            } else if (array_key_exists('title', $tag)) {
+                $db_tag = $this->tagAdminService->findBy('title', '=', $tag['title'])
+                                                ->first();
+                if (!$db_tag) {
                     $tag = $this->tagAdminService->store(Helper::collect($tag));
-                }
-                else
-                {
+                } else {
                     $tag = $db_tag;
                 }
-            }
-            else
-            {
+            } else {
                 $tag = $this->tagAdminService->store(Helper::collect($tag));
             }
 
             $tag_ids[] = $tag['id'];
         }
 
-        if(count($tag_ids))
-        {
+        if (count($tag_ids)) {
             $this->itemTagMapAdminService->storeKeysMap(Helper::collect([
-                'item_id'   => $item_id,
-                'tag_ids'   => $tag_ids,
+                'item_id' => $item_id,
+                'tag_ids' => $tag_ids,
             ]));
         }
     }
