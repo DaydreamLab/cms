8318cce209e18d24d030818e8476a8172d6fcedb
aass5050 <aass5050@gmail.com>
2021-03-23 15:26:24 +0800

    封裝一個Cut support class處理分詞

--- src/Models/FullTextSearchCast.php
+++ src/Models/FullTextSearchCast.php
@@ -2,8 +2,7 @@
 
 namespace DaydreamLab\Cms\Models;
 
-use Fukuball\Jieba\Finalseg;
-use Fukuball\Jieba\Jieba;
+use DaydreamLab\Cms\Support\Cut;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class FullTextSearchCast implements CastsAttributes
@@ -27,27 +26,7 @@ class FullTextSearchCast implements CastsAttributes
         if ($value == '')
             return $value;
 
-        Jieba::init();
-        Finalseg::init();
-        Jieba::loadUserDict(base_path('/user_dict.txt'));
-        ini_set('memory_limit', '1024M');
-
-        // 替換特殊字元＋移除html標籤
-        $str = str_replace(
-            array("\n", "\r\n", '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*',
-                  '+', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '-',
-                  '_', '`', '{', '|', '}', '~', '；', '﹔', '︰', '﹕', '：', '，',
-                  '﹐', '、', '．', '﹒', '˙', '·', '。', '？', '！', '～', '‥', '‧',
-                  '′', '〃', '〝', '〞', '‵', '‘', '’', '『', '』', '「', '」', '“',
-                  '”', '…', '❞', '❝', '﹁', '﹂', '﹃', '﹄'),
-            ' ',
-            strip_tags($value));
-
-        // 將處理過的字串做適用於搜尋的分詞
-        $cut = Jieba::cutForSearch($str);
-        unset ($str);
-
-        return implode(' ', $cut);
+        return Cut::cutForSearch($value, ['loadUserDict' => base_path('user_dict.txt')]);
     }
 
 
---
+++ src/Support/Cut.php
@@ -0,0 +1,83 @@
+<?php
+
+
+namespace DaydreamLab\Cms\Support;
+
+
+use Fukuball\Jieba\Finalseg;
+use Fukuball\Jieba\Jieba;
+
+class Cut
+{
+    /**
+     * 初始化jieba分詞系統
+     * @param array $options
+     * $options['init' => [$args...]]     傳入初始化Jieba參數
+     * $options['finalSeg' => [$args...]] 傳入初始化finalSeg參數
+     * $options['loadUserDict' => $path]  傳入自定義擴充字典位置
+     */
+    private static function init(array $options = [])
+    {
+        isset($options['init'])
+            ? Jieba::init($options['init'])
+            : Jieba::init();
+
+        isset($options['finalSeg'])
+            ? Finalseg::init($options['finalSeg'])
+            : Finalseg::init();
+
+        isset($options['loadUserDict'])
+            ? Jieba::loadUserDict($options['loadUserDict'])
+            : null;
+    }
+
+    /**
+     * 適用於搜尋系統的分詞
+     * @param $value
+     * @param array $options
+     * @param bool $implode
+     * @return array|string
+     */
+    public static function cutForSearch($value, array $options = [], bool $implode = true)
+    {
+        self::init($options);
+        $cut = Jieba::cutForSearch(self::replaceSign($value));
+
+        return $implode ? implode(' ', $cut) : $cut;
+    }
+
+    /**
+     * 一般分詞
+     * @param $value
+     * @param array $options
+     * @param bool $implode
+     * @return array|string
+     */
+    public static function cut($value, array $options = [], bool $implode = true)
+    {
+        self::init($options);
+        $cut = Jieba::cut(self::replaceSign($value));
+
+        return $implode ? implode(' ', $cut) : $cut;
+    }
+
+    /**
+     * 替換特殊字元＋移除html標籤
+     * @param $value
+     * @return string
+     */
+    public static function replaceSign($value)
+    {
+        return str_replace(
+            array("\n", "\r\n", '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*',
+                  '+', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '-',
+                  '_', '`', '{', '|', '}', '~', '；', '﹔', '︰', '﹕', '：', '，',
+                  '﹐', '、', '．', '﹒', '˙', '·', '。', '？', '！', '～', '‥', '‧',
+                  '′', '〃', '〝', '〞', '‵', '‘', '’', '『', '』', '「', '」', '“',
+                  '”', '…', '❞', '❝', '﹁', '﹂', '﹃', '﹄'),
+            ' ',
+            strip_tags($value));
+    }
+
+
+}
