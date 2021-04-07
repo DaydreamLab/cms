b09d73a459cd88a6e5053d2904eca00f0063aad2
aass5050 <aass5050@gmail.com>
2021-04-07 14:02:28 +0800

    若不使用全文索引查找則不加入full_text_search field

--- src/database/migrations/2021_03_18_041935_add_column_search_fulltext_to_items_table.php
+++ src/database/migrations/2021_03_18_041935_add_column_search_fulltext_to_items_table.php
@@ -13,13 +13,15 @@ class AddColumnSearchFulltextToItemsTable extends Migration
      */
     public function up()
     {
-        Schema::table('items', function (Blueprint $table) {
-            $table->text('full_text_search')->nullable()->after('description');
-        });
+        // 如果有配置要使用全文索引，才加入 full_text_search field
+        if (config('cms.use_word_segmentation')) {
+            Schema::table('items', function (Blueprint $table) {
+                $table->text('full_text_search')->nullable()->after('description');
+            });
 
-        // 增加全文檢索索引到full_text_search上
-        \Illuminate\Support\Facades\DB::statement('ALTER TABLE items ADD FULLTEXT INDEX fulltext_index (full_text_search)');
-
+            // 增加全文檢索索引到full_text_search上
+            \Illuminate\Support\Facades\DB::statement('ALTER TABLE items ADD FULLTEXT INDEX fulltext_index (full_text_search)');
+        }
     }
 
     /**
