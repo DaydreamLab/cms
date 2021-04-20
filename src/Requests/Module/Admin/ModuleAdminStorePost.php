4d1cc595c1e5dbd7c2888882fcae74e8cfd487e5
Jordan <jordan@daydream-lab.com>
2021-04-20 13:03:23 +0800

    fix: bug

--- src/Requests/Module/Admin/ModuleAdminStorePost.php
+++ src/Requests/Module/Admin/ModuleAdminStorePost.php
@@ -3,6 +3,7 @@
 namespace DaydreamLab\Cms\Requests\Module\Admin;
 
 use DaydreamLab\Cms\Requests\Module\ModuleStorePost;
+use DaydreamLab\JJAJ\Helpers\Helper;
 
 class ModuleAdminStorePost extends ModuleStorePost
 {
@@ -27,5 +28,48 @@ class ModuleAdminStorePost extends ModuleStorePost
             //
         ];
         return array_merge($rules, parent::rules());
+    }
+
+
+    public function rulesInput()
+    {
+        $rulesInput =  parent::rulesInput();
+
+        $params = $rulesInput->get('params');
+
+        if (isset($params['category_ids'])) {
+            $params['category_ids'] = collect($params['category_ids'])->pluck('id')->all();
+            if (!isset($params['with_items'])) {
+                $params['with_items'] = 'all';
+            } else {
+                $params['with_items'] = $params['with_items'] ?: 'all';
+            }
+            $params['order_by'] = 'ordering';
+            $params['order'] = 'desc';
+        }
+
+        if (isset($params['featured_display'])) {
+            $params['featured_display'] = (int) $params['featured_display'];
+        }
+
+        if (isset($params['featured_count'])) {
+            $params['featured_count'] = (int) $params['featured_count'];
+        }
+
+        if (isset($params['item_count'])) {
+            $params['item_count'] = (int) $params['item_count'];
+        }
+
+        if (isset($params['split_items_by_categories'])) {
+            $params['split_items_by_categories'] = (int) $params['split_items_by_categories'];
+        }
+
+        if (isset($params['split_items_by_featured'])) {
+            $params['split_items_by_featured'] = (int) $params['split_items_by_featured'];
+        }
+
+        $rulesInput->put('params', $params);
+
+        return $rulesInput;
     }
 }
