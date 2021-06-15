fd9ac86123d9be54e012a63e43c23ff119b13703
Jordan <ppuff1988@gmail.com>
2021-06-15 12:31:34 +0800

    temp

--- database/seeders/ZeroneAssetsGroupTableSeeder.php
+++ database/seeders/ZeroneAssetsGroupTableSeeder.php
@@ -3,13 +3,13 @@
 namespace DaydreamLab\Cms\Database\Seeders;
 
 use DaydreamLab\User\Models\Api\Api;
+use DaydreamLab\User\Models\User\UserGroup;
 use DaydreamLab\User\Services\Asset\Admin\AssetAdminService;
 use DaydreamLab\User\Services\Asset\Admin\AssetGroupAdminService;
 use Illuminate\Database\Seeder;
 
 class ZeroneAssetsGroupTableSeeder extends Seeder
 {
-
     /**
      * Run the database seeds.
      *
@@ -17,10 +17,14 @@ class ZeroneAssetsGroupTableSeeder extends Seeder
      */
     public function run()
     {
+        # 處理 AssetGroup、Asset、Api 間的關係
         $data = getJson(__DIR__ . '/jsons/zerone.json', true);
         foreach ($data as $inputData) {
             $inputAssets = $inputData['assets'];
             unset($inputData['assets']);
+            $inputUserGroups = $inputData['userGroups'];
+            unset($inputData['userGroups']);
+
             $assetGroup = app(AssetGroupAdminService::class)->store(collect($inputData));
             $assetIds = [];
             foreach ($inputAssets as $inputAsset) {
@@ -54,6 +58,13 @@ class ZeroneAssetsGroupTableSeeder extends Seeder
 
                 $assetIds[] = $asset->id;
             }
+            # 套用到Asset
+            $assetGroup->assets()->attach($assetIds);
+
+            # 套用到使用者群組
+            $userGroups = UserGroup::whereIn('title', $inputUserGroups)->get();
+            $assetGroup->userGroups()->attach($userGroups->pluck('id'));
         }
     }
+
 }
