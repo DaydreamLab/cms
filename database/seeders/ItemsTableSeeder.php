e71674ab85b03cba1a521182f7eee58f62180b93
Jerry <Jerry@daydream-lab.com>
2021-06-28 14:55:51 +0800

    temp

--- database/seeders/ItemsTableSeeder.php
+++ database/seeders/ItemsTableSeeder.php
@@ -2,7 +2,8 @@
 
 namespace DaydreamLab\Cms\Database\Seeders;
 
-use DaydreamLab\Cms\Models\Category\Category;
+use DaydreamLab\Cms\Services\Category\Admin\CategoryAdminService;
+use DaydreamLab\Cms\Services\Item\Admin\ItemAdminService;
 use DaydreamLab\JJAJ\Helpers\Helper;
 use Illuminate\Database\Seeder;
 
@@ -15,36 +16,21 @@ class ItemsTableSeeder extends Seeder
 
     public function run()
     {
-
+        $data = json_decode(file_get_contents(__DIR__.'/jsons/item.json'), true);
+        $this->categoryService = app(CategoryAdminService::class);
+        $this->itemAdminService = app(ItemAdminService::class);
+        foreach ($data as $category_data) {
+            $category = $this->categoryService->findBy('alias', '=', $category_data['alias'])->first();
+            foreach ($category_data['item'] as $item_data) {
+                $item_data['category_id'] = $category->id;
+                $i = $this->itemAdminService->store(collect($item_data));
+            }
+        }
     }
 
 
     public function migrate($data, $parent)
     {
-        foreach ($data as $category)
-        {
-            $childern = $category['children'];
-            $items    = $category['items'];
-            unset($category['children']);
-            unset($category['items']);
 
-            $category = $this->categoryService->store(Helper::collect($category));
-
-            foreach ($items as $item)
-            {
-                $item['category_id'] = $category->id;
-                $this->itemAdminService->store(Helper::collect($item));
-            }
-
-            if ($parent)
-            {
-                $parent->appendNode($category);
-            }
-
-            if (count($childern))
-            {
-                self::migrate($childern, $category);
-            }
-        }
     }
 }
