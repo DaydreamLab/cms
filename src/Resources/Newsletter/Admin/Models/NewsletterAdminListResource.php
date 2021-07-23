394cd0df7cb8dc6555c8776c984a5f3951fa8539
Jerry <Jerry@daydream-lab.com>
2021-07-23 18:15:38 +0800

    update

--- src/Resources/Newsletter/Admin/Models/NewsletterAdminListResource.php
+++ src/Resources/Newsletter/Admin/Models/NewsletterAdminListResource.php
@@ -21,7 +21,12 @@ class NewsletterAdminListResource extends BaseJsonResource
             'category_title'    => $this->newsletterCategory->title,
             'state'             => $this->state,
             'title'             => $this->title,
-            'publish_up'        => $this->getDateTimeString($this->locked_at, config('daydreamlab.cms.timezone')),
+            'created_at'                => $this->getDateTimeString($this->created_at, $tz),
+            'updated_at'                => $this->getDateTimeString($this->updated_at, $tz),
+            'locked_at'                 => $this->getDateTimeString($this->locked_at, $tz),
+            'creatorName'               => $this->creatorName,
+            'updaterName'               => $this->updaterName,
+            'lockerName'                => $this->lockerName,
             'locker'            => ($this->locker) ? $this->locker->only(['id', 'uuid', 'name']) : []
         ];
     }
