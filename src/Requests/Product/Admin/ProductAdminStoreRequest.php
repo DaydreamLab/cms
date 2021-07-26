5470838ce3682814fc292605e77ad8f25e8b0ba9
Jerry <Jerry@daydream-lab.com>
2021-07-26 18:44:40 +0800

    fix bug

--- src/Requests/Product/Admin/ProductAdminStoreRequest.php
+++ src/Requests/Product/Admin/ProductAdminStoreRequest.php
@@ -2,6 +2,7 @@
 
 namespace DaydreamLab\Cms\Requests\Product\Admin;
 
+use Carbon\Carbon;
 use DaydreamLab\Cms\Helpers\RequestHelper;
 use DaydreamLab\JJAJ\Requests\AdminRequest;
 use Illuminate\Validation\Rule;
