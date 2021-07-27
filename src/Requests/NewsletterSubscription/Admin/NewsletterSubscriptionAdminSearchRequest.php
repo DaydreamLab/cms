c9c1eadcbe7eed7be233a2d12a197807941578cc
Jerry <Jerry@daydream-lab.com>
2021-07-27 17:47:43 +0800

    fix

--- src/Requests/NewsletterSubscription/Admin/NewsletterSubscriptionAdminSearchRequest.php
+++ src/Requests/NewsletterSubscription/Admin/NewsletterSubscriptionAdminSearchRequest.php
@@ -3,6 +3,7 @@
 namespace DaydreamLab\Cms\Requests\NewsletterSubscription\Admin;
 
 use DaydreamLab\Cms\Requests\CmsSearchPost;
+use Illuminate\Validation\Rule;
 
 class NewsletterSubscriptionAdminSearchRequest extends CmsSearchPost
 {
@@ -27,7 +28,12 @@ class NewsletterSubscriptionAdminSearchRequest extends CmsSearchPost
     public function rules()
     {
         $rules = [
-            //
+            'state'         => [
+                'nullable',
+                'integer',
+                Rule::in([0,1,-1,-2])
+            ],
+            'newsletter_category'   => 'nullable|integer'
         ];
 
         return array_merge(parent::rules(), $rules);
@@ -37,6 +43,18 @@ class NewsletterSubscriptionAdminSearchRequest extends CmsSearchPost
     public function validated()
     {
         $validated = parent::validated();
+
+        if ($validated->get('state') == '') {
+            $validated->forget('state');
+            $validated['q'] = $this->q->whereIn('state', [0, 1]);
+        }
+
+        if ( $category_id = $validated->get('newsletter_category') ) {
+            $validated['q'] = $this->q->whereHas('newsletterCategories', function ($query) use ($category_id) {
+                $query->where('newsletter_subscription_newsletter_category_maps.category_id', '=', $category_id);
+            });
+        }
+        $validated->forget('newsletter_category');
 
         return $validated;
     }
--- src/Resources/NewsletterSubscription/Admin/Models/NewsletterSubscriptionAdminListResource.php
+++ src/Resources/NewsletterSubscription/Admin/Models/NewsletterSubscriptionAdminListResource.php
@@ -26,6 +26,8 @@ class NewsletterSubscriptionAdminListResource extends BaseJsonResource
             'newsletterCategories' => $this->newsletterCategories->map(function ($n) {
                 return $n->only(['id', 'alias', 'title']);
             }),
+            'company'           => ($this->company) ? $this->company->name : '',
+            'state'             => $this->state,
             'created_at'        => $this->getDateTimeString($this->created_at, $tz),
             'updated_at'        => $this->getDateTimeString($this->updated_at, $tz),
             'locked_at'         => $this->getDateTimeString($this->locked_at, $tz),
