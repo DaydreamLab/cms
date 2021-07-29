00588f346614839dba41b05c539ae15267d3bcc7
Jerry <Jerry@daydream-lab.com>
2021-07-29 12:31:31 +0800

    product front search

--- src/Requests/Product/Front/ProductFrontSearchRequest.php
+++ src/Requests/Product/Front/ProductFrontSearchRequest.php
@@ -2,6 +2,7 @@
 
 namespace DaydreamLab\Cms\Requests\Product\Front;
 
+use DaydreamLab\Cms\Models\ProductCategory\ProductCategory;
 use DaydreamLab\Cms\Requests\CmsSearchPost;
 
 class ProductFrontSearchRequest extends CmsSearchPost
@@ -24,7 +25,8 @@ class ProductFrontSearchRequest extends CmsSearchPost
     public function rules()
     {
         $rules = [
-            //
+            'brand_id'              => 'nullable|integer',
+            'product_category_id'   => 'nullable|integer'
         ];
 
         return array_merge(parent::rules(), $rules);
@@ -34,6 +36,21 @@ class ProductFrontSearchRequest extends CmsSearchPost
     public function validated()
     {
         $validated = parent::validated();
+
+        if ($brand_id = $validated->get('brand_id') ) {
+            $validated['q'] = $this->q->whereHas('brands', function ($query) use ($brand_id) {
+                $query->where('brands_products_maps.brand_id', '=', $brand_id);
+            });
+        }
+        $validated->forget('brand_id');
+
+        if ($category_id = $validated->get('product_category_id')) {
+            $p_c = ProductCategory::where('id', $category_id)->first();
+            $p_c_ids = $p_c->descendants()->get()->map(function ($p) { return $p->id; })->toArray();
+            $p_c_ids[] = $p_c->id;
+            $validated['q'] = $this->q->whereIn('product_category_id', $p_c_ids);
+        }
+        $validated->forget('product_category_id');
 
         return $validated;
     }
