246558090ef1c3507b013e9edb55b96ee566aafa
Jerry <Jerry@daydream-lab.com>
2021-08-23 12:18:27 +0800

    rma temp

--- src/Controllers/Rma/RmaController.php
+++ src/Controllers/Rma/RmaController.php
@@ -3,12 +3,14 @@
 namespace DaydreamLab\Cms\Controllers\Rma;
 
 use DaydreamLab\Cms\Requests\Rma\RmaSearchPost;
+use DaydreamLab\JJAJ\Traits\ApiJsonResponse;
 use Illuminate\Routing\Controller;
 use SoapClient;
 use Throwable;
 
 class RmaController extends Controller
 {
+    use ApiJsonResponse;
 
     public function search(RmaSearchPost $request)
     {
@@ -24,17 +26,35 @@ class RmaController extends Controller
         );
 
         $res = $client->__soapCall('checkProcess', ['parameters' => $params]);
-        if ($res->checkProcessResult != '查無資料') {
+        if ($res->checkProcessResult == '查無資料') {
+
+        } else {
             $repairList = explode(',', $res->checkProcessResult);
             unset($repairList[0]); # test;test
+            $response = [];
             foreach ($repairList as $repair) {
                 $p = explode(';', $repair);
                 print_r('編號：'.$p[0].'日期：'.$p[1]."\n");
                 $detail = $client->__soapCall('checkDetail', ['parameters' => ['srmano' => $p[0]]]);
                 print_r($detail->checkDetailResult."\n");
+                if (strpos($detail->checkDetailResult, '@#') !== false) {
+                    $detailList = explode('@#', $detail->checkDetailResult);
+                    $formedData = [];
+                    foreach ($detailList as $key => $item) {
+                        switch ($key) {
+                            case 0:
+                                $formedData['date'] = $item;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    $response[] = $formedData;
+                }
             }
         }
 
+        return $this->response('GetListSuccess', $response);
     }
 
 }
