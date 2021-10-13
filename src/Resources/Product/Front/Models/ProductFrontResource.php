39004c41b567b4088b8f82136f36f8f9d35367c7
ntut-mika <t107590003@ntut.org.tw>
2021-10-13 15:46:08 +0800

    fixed：非經銷會員應無法取得經銷價

--- src/Resources/Product/Front/Models/ProductFrontResource.php
+++ src/Resources/Product/Front/Models/ProductFrontResource.php
@@ -14,13 +14,29 @@ class ProductFrontResource extends BaseJsonResource
      */
     public function toArray($request)
     {
+        $product_data = $this->product_data;
+        $user = $request->user('api');
+
+        // 非經銷會員將 distributePrice 拔掉
+        if ($user) {
+            $group = $user->groups()->where('title', '=', '經銷會員')->first();
+        } else {
+            $group = null;
+        };
+
+        if (! $group) {
+            foreach ($product_data as &$product) {
+                unset($product['distributePrice']);
+            }
+        }
+
         return [
             'title'             => $this->title,
             'alias'             => $this->alias,
             'description'       => $this->description,
             'brands'            => $this->brands,
             'category'          => $this->category,
-            'product_data'      => $this->product_data,
+            'product_data'      => $product_data,
             'params'            => $this->params
         ];
     }
