b50f50b9b22473539c55b17dd91a060627103013
Jordan <jordan@daydream-lab.com>
2021-11-02 14:11:05 +0800

    fix: 修正前台統編資料不存在時不報錯

--- src/Requests/Setting/Front/SettingFrontGetItemGet.php
+++ src/Requests/Setting/Front/SettingFrontGetItemGet.php
@@ -6,6 +6,7 @@ use DaydreamLab\Cms\Requests\ComponentBase\CmsGetItemRequest;
 
 class SettingFrontGetItemGet extends CmsGetItemRequest
 {
+    protected $needAuth = false;
     /**
      * Determine if the user is authorized to make this request.
      *
