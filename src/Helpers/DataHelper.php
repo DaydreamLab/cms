5dbcb04a71fc9b18f58c41625952c031768279eb
Jordan <jordan@daydream-lab.com>
2022-07-28 18:43:28 +0800

    fix: bug

--- src/Helpers/DataHelper.php
+++ src/Helpers/DataHelper.php
@@ -4,7 +4,7 @@ namespace DaydreamLab\Cms\Helpers;
 
 class DataHelper
 {
-    const CUSTOMER_MESSAGE_TYPES =  ['產品詢價', '客訴反應', '合作提案', '技術問題', '零壹課程', '其他', '帳號登入問題', '客訴反應'];
+    const CUSTOMER_MESSAGE_TYPES =  ['產品詢價', '客訴反應', '合作提案', '技術問題', '零壹課程', '其他', '帳號登入問題'];
     const CUSTOMER_MESSAGE_STATUS =  ['待處理', '處理中', '已處理', '不需處理'];
     const cities_order = ['基隆市', '臺北市', '新北市', '桃園市', '新竹縣', '新竹市','苗栗縣', '臺中市', '彰化縣',
         '南投縣', '雲林縣', '嘉義縣', '嘉義市', '臺南市', '高雄市','屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '澎湖縣',
