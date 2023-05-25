57357fcdd75a1c867fb54d2cd631f7f499ded78b
Jordan <jordan@daydream-lab.com>
2023-05-25 16:15:37 +0800

    fix: 調整extrafield 找不到就不做query

--- src/Traits/WithExtrafield.php
+++ src/Traits/WithExtrafield.php
@@ -15,64 +15,45 @@ trait WithExtrafield
 {
     protected $class_extrafield_group = null;
 
-    public function extrafieldGroup()
-    {
-        return $this->hasOne($this->getExtrafieldGroupClass(), 'id', 'extrafield_group_id');
-    }
-
-
     public function getExtrafieldsAttribute($value)
     {
         $value = $value ? $value : json_encode([]);
         $extrafield_ids = [];
-        foreach (json_decode($value) as $extrafield)
-        {
+        foreach (json_decode($value) as $extrafield) {
             $extrafield_ids[] = $extrafield->id;
         }
 
-
-        if($this->model_type == 'parent')
-        {
+        if (!count($extrafield_ids)) {
+            $extrafields_data = collect();
+        } elseif ($this->model_type == 'parent') {
             $extrafields_data = Extrafield::whereIn('id', $extrafield_ids)->get();
-        }
-        elseif($this->model_type == 'front')
-        {
+        } elseif ($this->model_type == 'front') {
             $extrafields_data = ExtrafieldFront::whereIn('id', $extrafield_ids)->get();
-        }
-        else
-        {
+        } else {
             $extrafields_data = ExtrafieldAdmin::whereIn('id', $extrafield_ids)->get();
         }
 
         // 組合實際的資料與額外欄位的初始定義
         $data = (object)[];
-        foreach ($extrafields_data as $extrafield_data)
-        {
-            foreach (json_decode($value) as $extrafield)
-            {
-                if($extrafield->id == $extrafield_data->id) {
-                    $extrafield_data->value  = $extrafield->value;
+        foreach ($extrafields_data as $extrafield_data) {
+            foreach (json_decode($value) as $extrafield) {
+                if ($extrafield->id == $extrafield_data->id) {
+                    $extrafield_data->value = $extrafield->value;
                     $extrafield_data->params = empty($extrafield->params) ? $extrafield_data->params : $extrafield->params;
                 }
             }
 
-            if($this->model_type == 'parent')
-            {
-                $data->{$extrafield_data->id} = (object) $extrafield_data;
-            }
-            elseif($this->model_type == 'front')
-            {
-                $data->{$extrafield_data->alias} = (object) $extrafield_data;
-            }
-            else
-            {
-                $data->{$extrafield_data->id} = (object) $extrafield_data;
+            if ($this->model_type == 'parent') {
+                $data->{$extrafield_data->id} = (object)$extrafield_data;
+            } elseif ($this->model_type == 'front') {
+                $data->{$extrafield_data->alias} = (object)$extrafield_data;
+            } else {
+                $data->{$extrafield_data->id} = (object)$extrafield_data;
             }
         }
 
         return $data;
     }
-
 
     public function getExtrafieldGroupTitleAttribute()
     {
@@ -81,21 +62,19 @@ trait WithExtrafield
         return $group ? $group->title : null;
     }
 
+    public function extrafieldGroup()
+    {
+        return $this->hasOne($this->getExtrafieldGroupClass(), 'id', 'extrafield_group_id');
+    }
 
     public function getExtrafieldGroupClass()
     {
-        if($this->class_extrafield_group == null)
-        {
-            if($this->model_type == 'parent')
-            {
+        if ($this->class_extrafield_group == null) {
+            if ($this->model_type == 'parent') {
                 $this->class_extrafield_group = ExtrafieldGroup::class;
-            }
-            elseif ($this->model_type == 'front')
-            {
+            } elseif ($this->model_type == 'front') {
                 $this->class_extrafield_group = ExtrafieldGroupFront::class;
-            }
-            else
-            {
+            } else {
                 $this->class_extrafield_group = ExtrafieldGroupAdmin::class;
             }
         }
@@ -108,8 +87,7 @@ trait WithExtrafield
     {
         $data = [];
 
-        foreach ($value as $item)
-        {
+        foreach ($value as $item) {
             $data[] = (object)$item;
         }
 
